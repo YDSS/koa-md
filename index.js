523de8f557cbcf9c65067970e6819cb212bd2aec
YDSS <y.dsheng10@gmail.com>
2017-03-29 22:43:23 +0800

    finish without supporting caches

---
+++ index.js
@@ -0,0 +1,12 @@
+const Koa = require('koa');
+const md = require('./md');
+
+const app = new Koa();
+const port = 3000;
+
+app.use(async function (ctx, next) {
+    await md(ctx, '/test.md');
+});
+
+app.listen(port);
+console.log(`http://localhost:${port}`);
